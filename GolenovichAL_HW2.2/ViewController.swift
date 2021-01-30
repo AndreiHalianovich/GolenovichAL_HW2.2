493a8f5f7551ff258991fc024d740e7191ca4c70
Andrei Halianovich <igolenovichbox@gmail.com>
2021-01-30 18:36:00 +0300

    Some code for HW

--- GolenovichAL_HW2.2/ViewController.swift
+++ GolenovichAL_HW2.2/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // View
+    @IBOutlet var coloredView: UIView!
+    
+    // Labels with current value of sliders
+    @IBOutlet var redColorLabel: UILabel!
+    @IBOutlet var greenColorLabel: UILabel!
+    @IBOutlet var blueColorLabel: UILabel!
+    
+    // Sliders
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        coloredView.layer.cornerRadius = coloredView.frame.height / 10
     }
-
-
+    
+    @IBAction func slidersValueDidChanged(_ sender: UISlider) {
+        let red = redSlider.value
+        let green = greenSlider.value
+        let blue = blueSlider.value
+        
+        let color = UIColor(displayP3Red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
+        coloredView.backgroundColor = color
+        
+        redColorLabel.text = String(format: "%.02f", red)
+        greenColorLabel.text = String(format: "%.02f", green)
+        blueColorLabel.text = String(format: "%.02f", blue)
+    }
 }
-
