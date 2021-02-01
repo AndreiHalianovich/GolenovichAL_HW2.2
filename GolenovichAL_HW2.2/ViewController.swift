70047fcce1ec609177c3f010cf8169663ec3ecd2
Andrei Halianovich <igolenovichbox@gmail.com>
2021-02-01 20:48:12 +0300

    add new method

--- GolenovichAL_HW2.2/ViewController.swift
+++ GolenovichAL_HW2.2/ViewController.swift
@@ -8,36 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    // View
+    
     @IBOutlet var coloredView: UIView!
     
-    // Labels with current value of sliders
     @IBOutlet var redColorLabel: UILabel!
     @IBOutlet var greenColorLabel: UILabel!
     @IBOutlet var blueColorLabel: UILabel!
     
-    // Sliders
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         coloredView.layer.cornerRadius = coloredView.frame.height / 10
-        coloredView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+        
+        setColor()
+        
+        redColorLabel.text = String(format: "%.02f", redSlider.value)
+        greenColorLabel.text = String(format: "%.02f", greenSlider.value)
+        blueColorLabel.text = String(format: "%.02f", blueSlider.value)
     }
     
     @IBAction func slidersValueDidChanged(_ sender: UISlider) {
-        let red = CGFloat(redSlider.value)
-        let green = CGFloat(greenSlider.value)
-        let blue = CGFloat(blueSlider.value)
+        setColor()
         
-        let color = UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1.0)
-        coloredView.backgroundColor = color
-        
-        redColorLabel.text = String(format: "%.02f", red)
-        greenColorLabel.text = String(format: "%.02f", green)
-        blueColorLabel.text = String(format: "%.02f", blue)
+        redColorLabel.text = String(format: "%.02f", redSlider.value)
+        greenColorLabel.text = String(format: "%.02f", greenSlider.value)
+        blueColorLabel.text = String(format: "%.02f", blueSlider.value)
+    }
+    
+    private func setColor() {
+        coloredView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value),
+                                              green: CGFloat(greenSlider.value),
+                                              blue: CGFloat(blueSlider.value),
+                                              alpha: 1.0)
     }
 }
